8c068b64d9c28e2a1f125aa493fafb973487353f
Alamahant <alamahant@gmail.com>
2026-05-17 16:44:34 +0300

    update to v1.2.8

--- Constants.cpp
+++ Constants.cpp
@@ -2,7 +2,7 @@
 
 namespace JASMINE_CONSTANTS {
 
-const char* APP_VERSION = "1.2.7";
+const char* APP_VERSION = "1.2.8";
 const QString appDirPath = QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation) + "/Jasmine";
 const QString downloadsDirPath = appDirPath + "/Downloads";
 const QString screenshotsDirPath = appDirPath + "/Screenshots";
